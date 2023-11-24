23708fd4cbe18e74b090ca20b645dd7e3629f723
ddozza54 <bananamayong@gmail.com>
2023-11-24 15:43:22 +0900

    feat: 모든 억만장자 프로필 불러오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,38 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react'
+
+interface Billionaire {
+  id: string;
+  industries: []
+  name: string;
+  netWorth: number;
+  squareImage: string;
+}
+
 export default function Home() {
+  const [billionaires, setBillionaires] = useState<Billionaire[]>();
+  useEffect(() => {
+    (async () => {
+      const result = await (await fetch('https://billions-api.nomadcoders.workers.dev/')).json()
+      setBillionaires(result);
+    })()
+  }, [])
+  console.log(billionaires)
   return (
-    <>Home</>
+    <div
+      className='flex flex-wrap bg-zinc-800'
+    >{
+        billionaires?.map(billionaire =>
+          <div
+            className='flex flex-col p-4'
+            key={billionaire.id}>
+            <Link href={`person/${billionaire.id}`}>
+              < img src={`${billionaire.squareImage}.png`} />
+              <h4 className='text-white font-bold text-lg'>{billionaire.name}</h4>
+              <span className='text-white '>{billionaire.netWorth}/{billionaire.industries}</span>
+            </Link>
+          </div>
+        )
+      }</div >
   )
 }
